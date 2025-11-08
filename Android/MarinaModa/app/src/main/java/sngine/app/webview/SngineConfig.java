9b59408698a88b1581f3033502bd410107f20819
Dmitry Olegovich Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2025-11-08 15:16:21 +0300

    diff ..

--- Android/MarinaModa/app/src/main/java/sngine/app/webview/SngineConfig.java
+++ Android/MarinaModa/app/src/main/java/sngine/app/webview/SngineConfig.java
@@ -5,7 +5,7 @@ class SngineConfig {
     /* -- CONFIG VARIABLES -- */
 
     //complete URL of your Sngine website
-    static String Sngine_URL = "https://marina.rechain.network/";
+    static String Sngine_URL = "https://bemusic.1.vebto.com/";
 
     // OneSignal APP Id
     static String Sngine_ONESIGNAL_APP_ID = "";
